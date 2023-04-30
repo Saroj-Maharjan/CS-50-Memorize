0f96a3c31494126cf2c43c65df01a4de6380923f
Saroj Maharjan <saroj.maharjan1993@gmail.com>
2023-04-30 13:09:42 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by saroj maharjan on 30/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

680ce22f691abf5b0636a67f248e5ce241c1082b
Saroj Maharjan <saroj.maharjan1993@gmail.com>
2023-04-30 14:16:42 +0800

    View For The main Page

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,18 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
         }
-        .padding()
+        .foregroundColor(.red)
+        .padding(.horizontal)
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool
+    var body: some View {
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("🚗")
+                    .font(.largeTitle)
+            }else {
+                shape
+                    .fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
